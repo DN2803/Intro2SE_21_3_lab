3bba5971cfdad863a1cc78fd5023f54d8e953e15
dn28203 <ntkngann2k3@gmail.com>
2023-12-09 21:47:41 +0700

    add PatientDetail

--- src/frontend/src/views/loggedpages/doctoruser/PatientDetail.jsx
+++ src/frontend/src/views/loggedpages/doctoruser/PatientDetail.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { Button, Row, Col } from "reactstrap";
+import { Button, Row, Col, Container } from "reactstrap";
+import '../../../styles/PatientDetail.scss'
 const PatientDetail = () => {
     const { idpatient } = useParams();
     // get medical history from database 
     // get information from database where patient's id = id patient
 
+    // table chứa lịch sửu khám 
+    const title_medical_history = [
+        {Header: 'STT'},
+        {Header: "Ngày khám"},
+        {Header: "Bác sĩ"},
+        {Header: "Chuẩn đoán"},
+        {Header: "Đơn thuốc"},
+    ];
+    const title_prescription = [
+        {Header: 'Tên thuốc'},
+        {Header: 'Số lượng'}
+    ];
 
 
     return (
         <>
+        <Container className="detail">
         <h1>#{idpatient}</h1>
-        <Button className="create-prescription">Tạo đơn thuốc</Button>
+        
         <div className="patient">
+            <label>Thông tin bệnh nhân</label>
             <div className="patient-information">
                 <Row>
                     <Col>
@@ -50,13 +65,25 @@ const PatientDetail = () => {
                 </Row>
                 
             </div>
+            <Button className="create-prescription">Tạo đơn thuốc</Button>
             <div className="patient-history">
             <label>Lịch sử khám</label>
+            
+            <table>
+                <thead>
+                    {
+                        title_medical_history.map(t => {
+                            return (<td key={t}>{t.Header}</td>)
+                        })
+                    }
+                    
 
-                
+                </thead>
+            </table>
 
             </div>
         </div>
+        </Container>
         </>
     )
 }
