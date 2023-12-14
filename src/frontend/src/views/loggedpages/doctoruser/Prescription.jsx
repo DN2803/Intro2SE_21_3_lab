4d05550c4f7a48f5ae522b31004ea80d9a54bda5
dn28203 <ntkngann2k3@gmail.com>
2023-12-14 21:08:29 +0700

    done prescription ui

--- src/frontend/src/views/loggedpages/doctoruser/Prescription.jsx
+++ src/frontend/src/views/loggedpages/doctoruser/Prescription.jsx
@@ -1,8 +1,9 @@
 import React, {useState} from "react";
-import { useParams } from "react-router-dom";
-import { Container, Button, Label} from "reactstrap";
+import { useParams ,  useNavigate} from "react-router-dom";
+import { Container, Button, Row, Col} from "reactstrap";
+import { FaPlus } from "react-icons/fa6";
 
-
+import '../../../styles/Prescription.scss'
 const Prescription = () => {
     const id = useParams();
     console.log(id);
@@ -15,7 +16,7 @@ const Prescription = () => {
     const formattedDate = currentDate.toLocaleDateString();
     const isNow = date === formattedDate.replace(/\//g, '-') ;
     console.log (doctor_id, patient_id, date);
-    const [inputFields, setInputFields] = useState(['']); // Danh sách trường nhập
+    const [inputFields, setInputFields] = useState([]); // Danh sách trường nhập
 
     const handleAddField = () => {
         setInputFields([...inputFields, '']); // Thêm một trường nhập mới vào danh sách
@@ -25,38 +26,68 @@ const Prescription = () => {
         const newInputFields = [...inputFields];
         newInputFields[index] = value;
         setInputFields(newInputFields);
+        
+    };
+    
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1); // Go back one step in the history stack
+    };
+    const save = () => {
+        
     };
     return (
         <>
         <Container className="detail">
             <h2>{date}</h2>
+            <Row>
             <div className="form">
-                <label>Bác sĩ khám</label>
-                <input type="text" />
-                <label>Triệu chứng</label>
-                <input type="text" />
-                <label>Chuẩn đoán</label>
-                <input type="text" />
+                <div className="form-doctorname">
+                    <label>Bác sĩ khám</label>
+                    <input type="text" value={doctor_id}/>
+                </div>
+                <div className="group-form">
+                    <label>Triệu chứng</label>
+                    <input type="text" />
+                </div>
+                <div className="group-form">
+                    <label>Chuẩn đoán</label>
+                    <input type="text" />
+                </div>
                 
             </div>
+            </Row>
+           
             <h3>Danh sách thuốc</h3>
             {inputFields.map((field, index) => (
                 <div key={index} className="input-pair">
                 <label>Tên thuốc</label>
                 <input
                   type="text"
-                  
+                  className="name-medicince"
                   onChange={(e) => handleChange(index, e.target.value)}
                 />
                 <label>Số lượng</label>
                 <input
                   type="number"
-                  
+                  className="quantity"
                   onChange={(e) => handleChange(index, e.target.value)}
                 />
               </div>
             ))}
-            {isNow &&<Button onClick={handleAddField}>Thêm thuốc</Button>}
+            {isNow &&<div className="text-wrapper"  onClick={handleAddField}> <FaPlus /> &nbsp;Thêm thuốc</div>}
+            <div className="button">
+            <Row>
+                <Col>
+                <Button className="btn primary__btn save" conClick ={save}> Lưu thay đổi</Button>
+                </Col>
+                <Col>
+                <Button className="btn outline__btn back " onClick={goBack}>Thoát</Button>
+                </Col>
+            </Row>
+        </div>
+        
             
 
         </Container>
