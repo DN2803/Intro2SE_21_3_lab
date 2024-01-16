b12b75e926347f0ec7ada30ebb822de35dc6110c
khuevo098 <khuevo098@gmail.com>
2024-01-16 13:38:42 +0700

    chỉnh sửa giao diện login

--- src/frontend/src/views/pages/Login.jsx
+++ src/frontend/src/views/pages/Login.jsx
@@ -44,14 +44,14 @@ const Login = () => {
                     <Col lg='8' className='m-auto'>
                         <div className="login_form d-flex align-items-center justify-content-center">
                             <Form onSubmit={handleSubmit}>
-                                <h3>Please login.</h3>
+                                <h3>Đăng nhập</h3>
                                 <div className="form-group ">
-                                    <label htmlFor="exampleInputEmail1">Username</label>
+                                    <label htmlFor="exampleInputEmail1">Tên đăng nhập</label>
                                     <input type="text" className="form-control" id="exampleInputEmail1" />
                                     
                                 </div>
                                 <div className="form-group ">
-                                    <label htmlFor="exampleInputPassword1">Password</label>
+                                    <label htmlFor="exampleInputPassword1">Mật khẩu</label>
                                     <input type="password" className="form-control" id="exampleInputPassword1" />
                                 </div>
                                 {
@@ -60,8 +60,8 @@ const Login = () => {
                                 </div>
                                 }
                                 <div className='form-group' style={{ marginTop: '2vh' }}>
-                                    <button type="submit" className="btn btn-primary">Submit</button>
-                                    &nbsp; <a href="/">Back to Home</a>
+                                    <button type="submit" className="btn btn-primary">Đăng nhập ngay</button>
+                                    &nbsp; <a href="/">Quay về Trang chính</a>
                                 </div>
                             </Form>
                         </div>
