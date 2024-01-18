8a12978ea7f2743f518e1e12b2d1f504b2d90198
khuevo098 <khuevo098@gmail.com>
2024-01-18 15:46:56 +0700

    hồi trước bỏ cái lấy bệnh nhân từ lịch hẹn cài MVC nhầm ở bệnh án, giờ chuyển qua MVC liên quan đến lịch hẹn

--- src/backend/controllers/appointmentController.js
+++ src/backend/controllers/appointmentController.js
@@ -66,3 +66,29 @@ exports.createAppointment = async (req, res) => {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
+
+
+exports.getListPatients = async (req, res) => {
+  try {
+    const doctorID = req.params.id
+    console.log(doctorID);
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
+    const day = currentDate.getDate().toString().padStart(2, "0");
+
+    const formattedDate = `${year}/${month}/${day}`;
+
+    console.log(formattedDate);
+    // Coi lại nên truyền tham số gì vào?
+    const patientsList = await AppointmentModel.getListPatientsFromAppointment(
+      formattedDate,
+      doctorID
+    );
+    console.log(patientsList);
+    return res.status(200).json({ patientsList });
+  } catch (error) {
+    console.error("Error: ", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
